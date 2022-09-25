5e72b34de2fdf490b0f113fd7195012f96c92e9b
Kamil Łaskarzewski <k4mlas@gmail.com>
2022-09-25 17:05:51 +0200

    Finish project

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
 
+const navBtnBars = document.querySelector('.burger__btn-bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
 
@@ -22,7 +26,30 @@ const handleNavItemsAnimation = () => {
 		item.style.animationDelay = '.' + delayTime + 's'; //Dla każdego item dodajemy styl liniowy opóźnieni animacji o 0s.
 		delayTime++; // dodajemy jedną sekundę w pętli
 	});
-	
 };
 
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (
+			!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
+
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
